76a8a278da56f5d09c5a317ebd7c6d02debc05c6
ZhaoTianhao <67959240+tianhaozhao2008@users.noreply.github.com>
2021-01-04 10:14:29 +0800

    Update and rename 用两个栈实现队列.java to 9、用两个栈实现队列.java

--- 刷题群/9、用两个栈实现队列.java
+++ 刷题群/9、用两个栈实现队列.java
@@ -19,5 +19,31 @@
 思路：第一开始想的是加入元素的时候就是放到栈A，取出元素的时候就把栈A的所有移动到栈B，然后取出；下次添加元素时再把栈B的挪到栈A再添加（这一步其实没必要了，直接放到A就行）
 
 看到评论说“如果你使用Stack的方式来做这道题，会造成速度较慢； 
-原因的话是Stack继承了Vector接口，而Vector底层是一个Object[]数组，那么就要考虑空间扩容和移位的问题了。 
+原因的话是Stack继承了Vector接口，而Vector底层是一个Object[]数组，那么就要考虑空间扩容和移位的问题了
+（Vector通过一个连续的数组存放元素，如果集合已满，在新增数据的时候，就要分配一块更大的内存，将原来的数据复制过来，释放之前的内存，在插入新增的元素；）。 
+
 可以使用LinkedList来做Stack的容器，因为LinkedList实现了Deque接口，所以Stack能做的事LinkedList都能做，其本身结构是个双向链表，扩容消耗少。”
+class CQueue {
+    Deque<Integer> stack1;
+    Deque<Integer> stack2;
+    public CQueue() {
+        stack1=new LinkedList<Integer>();
+        stack2=new LinkedList<Integer>();
+    }
+
+    public void appendTail(int value) {
+        stack1.push(value);
+    }
+
+    public int deleteHead() {
+        if(stack2.size()==0 && stack1.size()==0){
+            return -1;
+        }
+        if(stack2.size()==0 && stack1.size()!=0){
+            while(stack1.size()!=0){
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+}
