8bdeb9d3fe4d47bdbe05d65b711482a785fdb417
ZhaoTianhao <67959240+tianhaozhao2008@users.noreply.github.com>
2021-01-11 20:13:28 +0800

    Update 28、对称的二叉树.java

--- 刷题群/28、对称的二叉树.java
+++ 刷题群/28、对称的二叉树.java
@@ -5,7 +5,7 @@
 
 2.用bfs实现：先把root的两个子节点入队，然后出队——关键点是每次出队两个元素（我想到了bfs但是没想到要两个一起出队。这样才能继续再入队4个元素，才能使两两对称比较）
 这种方法运行较慢，bfs的队列和dfs的栈的出入容器次数是一样的，速度差别的原因可能jvm栈操作与LinkedList速度不同，以及涉及到内存寻址，局部性原理。
-bfs占的空间多应该是队列中存放的是节点对象的引用，而dfs的栈中存放的应当是代码块。
+两种方法空间复杂度相似，dfs递归的稍微小5%左右。
 class Solution {
     public boolean isSymmetric(TreeNode root) {
         if(root==null) return true;
@@ -31,3 +31,21 @@ class Solution {
 }
 
 3.分别对head节点的两个子树做dfs遍历（前序遍历），只不过一个是中左右，一个是中右左。（或者后续遍历也ok）
+class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) { val = x; }
+}
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root==null) return true;
+        return dfs(root.left,root.right);
+    }
+    boolean dfs(TreeNode a,TreeNode b){
+        if(a==null&&b==null)return true;
+        else if(a==null||b==null)return false;
+        else if(a.val!=b.val)return false;
+        else return dfs(a.left,b.right)&&dfs(a.right,b.left);
+    }
+}
