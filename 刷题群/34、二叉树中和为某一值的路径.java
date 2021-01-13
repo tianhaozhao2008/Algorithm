c13f757004b1b765f4af029d4ee5032ebfeb847c
ZhaoTianhao <67959240+tianhaozhao2008@users.noreply.github.com>
2021-01-13 20:34:15 +0800

    Update 34、二叉树中和为某一值的路径.java

--- 刷题群/34、二叉树中和为某一值的路径.java
+++ 刷题群/34、二叉树中和为某一值的路径.java
@@ -1,6 +1,8 @@
 这题关键是打印路径，比如遍历二叉树的话就很简单，但是打印所有从根节点到叶节点的路径，我就想了一两个小时也不会，我真是颓了，都开始用野对象去尝试了。
 方法是：弄一个全局变量的list来保存路径，然后遍历，把节点加入list，如果是叶节点就打印list。然后调用自身（左子节点和右子节点），调用完后把加入list的节点删去。
       这就是所谓回溯，即调用各个方向的自身的之前和之后的代码，完成相反的动作（如加入和删除、标记和去标记等）。
+
+一、先看下面这个最简单的打印二叉树所有路径。
 class TreeNod {
     int val;
     TreeNod left;
@@ -27,4 +29,26 @@ class Ceshi {
         if(node.right!=null)recur(node.right);
         res.remove(res.size()-1);
     }
+}
+
+二、再看那道题。（注意可能有负数所以不能小于0就结束）
+注意：把temp加入res时，不能直接加入，不然加入的是一个引用，之后该引用指向的对象会发生修改的，就乱套了。所以这里要新建一个副本new ArrayList<>(temp)
+class Solution {
+     List<List<Integer>> res=new ArrayList<>();
+     List<Integer> temp=new ArrayList<>();
+     public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        if(root==null)return res;
+        recur(root,sum);
+        return res;
+     }
+     void recur(TreeNode root,int sum){
+         temp.add(root.val);
+         sum=sum-root.val;
+         if(root.left==null&&root.right==null&&sum==0){
+             res.add(new ArrayList<>(temp));
+         }
+         if(root.left!=null)recur(root.left,sum);
+         if(root.right!=null)recur(root.right,sum);
+         temp.remove(temp.size()-1);
+     }
 }
