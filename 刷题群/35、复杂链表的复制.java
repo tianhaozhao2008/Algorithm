bbb108f418fe299ef7a6608da74733db7cd7f48d
ZhaoTianhao <67959240+tianhaozhao2008@users.noreply.github.com>
2021-01-14 11:02:11 +0800

    Update 35、复杂链表的复制.java

--- 刷题群/35、复杂链表的复制.java
+++ 刷题群/35、复杂链表的复制.java
@@ -1,4 +1,6 @@
 这题就是深拷贝链表。
+
+方法1：我的方法
 一开始我直接当成二叉树做了，就是递归，递归的参数是新建的new节点对象和要拷贝的节点，然后对左子节点和右子节点做递归。
 结果这题的ramdom指针的路子相当野，还可以往回指向从而形成无限循环，因此我第一次的代码超时了。
 
@@ -34,6 +36,27 @@ class Solution {
     }
 }
 
+方法2：题解的第一种方法，此时问题已经简单化了，我刚才没审清题，把问题复杂化了。题目要求是一个普通链表，然后加上random指针，random指针指向的只能是链表中的节点，
+而我把它当成也可以指向链表外的节点了。
+所以我刚才很疑惑dfs递归做的题为何也能用迭代做。。
+思路很简单：先遍历一遍，把原始链表的节点和拷贝的节点放入哈希表；再遍历一遍，根据哈希表中原始节点的信息，来把新节点之间的边画出来。
+class Solution {
+    Map<Node,Node>map=new HashMap<>();
+    public Node copyRandomList(Node head) {
+        Node cur=head;
+        while(cur!=null){
+            map.put(cur,new Node(cur.val));
+            cur=cur.next;
+        }
+        cur=head;
+        while(cur!=null){
+            map.get(cur).next=map.get(cur.next);
+            map.get(cur).random=map.get(cur.random);
+            cur=cur.next;
+        }
+        return map.get(head);
+    }
+}
 
 
 
