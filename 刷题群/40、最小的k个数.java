7f95f6a268417769e70d3f9fe80a851472166d73
ZhaoTianhao <67959240+tianhaozhao2008@users.noreply.github.com>
2021-01-16 21:17:47 +0800

    Update 40、最小的k个数.java

--- 刷题群/40、最小的k个数.java
+++ 刷题群/40、最小的k个数.java
@@ -24,6 +24,44 @@ class Solution {
     }
 }
 
-方法2：快排的改进版。每次分成两部分，如果那一部分的长度大于k，那么就在那一部分中继续分；如果那一部分的长度小于k，那么就去另一部分把剩下的几个也找出来。
+方法2：快排的改进版。每次分成两部分，如果那一部分的长度大于k，那么就在那一部分中继续分；如果那一部分的长度小于k，那么就去另一部分把剩下的几个也找出来。如果那一部分等于
+k，就不分了。
 空间复杂度O（1），时间复杂度期望值是O（n）（即n+n/2+n/4+n/8...即2n），最坏情况是每次分只能分出来一个来，就是O（n*n）。
 方法2的优势就是空间和时间复杂度，但方法二改动了原始数组，且当数据流过大时，比如内存都无法同时存放这么多数据时，方法2这种遍历数组的肯定就不行了。
+快速排序直接背吧，边界条件太麻烦，没时间慢慢试了。。https://github.com/tianhaozhao2008/Algorithm/blob/master/QuickSort.java
+
+这题邪门，最后一个测试用例就是过不了我擦，
+class Solution {
+    int[]arr;
+    public int[] getLeastNumbers(int[] arr, int k) {
+        if(k==0 ||arr.length==0)return new int[]{};
+        this.arr=arr;
+        partition(0,arr.length-1,k);
+        int []res=new int[k];
+        for(int i=0;i<k;i++){
+            res[i]=arr[i];
+        }
+        return res;
+    }
+    void partition(int left,int right,int k){
+        if(left>=right)return;
+        int tag=arr[(left+right)/2];
+        int i=left;
+        int j=right;
+        while(i<=j) {
+            while (arr[i] < tag) i++;
+            while (arr[j] > tag) j--;
+            if (i<=j) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        if(j-left+1==k)return;
+        else if(j-left+1>k) partition(left,j,k);
+        else partition(i,right,k-j+left-1);
+    }
+}
+
