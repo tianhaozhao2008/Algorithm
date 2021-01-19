54aaf13c7600dd9a08a4cb4027caf8a3835ad57e
ZhaoTianhao <67959240+tianhaozhao2008@users.noreply.github.com>
2021-01-19 22:21:55 +0800

    Update 51、数组中的逆序对.java

--- 刷题群/51、数组中的逆序对.java
+++ 刷题群/51、数组中的逆序对.java
@@ -13,3 +13,56 @@
 去的同时，看右边已经放进去几个了，此时放进去了2个说明有2个比5小，所以逆序对+2，然后是两个6放进去（如果一样就左边先放，不然会出错），逆序对再+2，
 然后7放进去，逆序对再+3，最后得到横跨前后部分的逆序对有7个。
 
+归并排序写了点bug，查了十几分钟才搞定，看来对归并还是不熟，再记一遍归并排序的细节吧，达到快速排序那么熟就行了。
+class Solution {
+    public static void main(String[] args) {
+        Solution solution=new Solution();
+        int x=solution.reversePairs(new int[]{7,5,6,4});
+        System.out.println(x);
+    }
+
+    public int reversePairs(int[] nums) {
+        if(nums.length==0)return 0;
+        return sort(nums,0,nums.length-1);
+    }
+
+    int sort(int[]num,int left,int right){
+        if(left==right)return 0;
+        int a=sort(num,left,(left+right)/2);
+        int b=sort(num,(left+right)/2+1,right);
+        int sum=0; //代表接下来横跨左右两部分的逆序对。
+        //接下来是归并,从小到大排序
+        int []temp=new int[right-left+1];
+        int i=left; int j=(left+right)/2+1;
+        for(int index=0;index<temp.length;index++){
+            //下面两条if是比如左边全读完了，就剩下右边了，就直接填，就不比了，不然越界。
+            if(i>(left+right)/2){
+                temp[index]=num[j];
+                j++;
+                continue;
+            }
+            if(j>right){
+                temp[index]=num[i];
+                i++;
+                sum+=j-((left+right)/2+1);
+                continue;
+            }
+            //比较左右部分的起始数值的大小。
+            if(num[i]<=num[j]){
+                temp[index]=num[i];
+                i++;
+                sum+=j-((left+right)/2+1);
+            }
+            else{
+                temp[index]=num[j];
+                j++;
+            }
+        }
+        for(i=0;i<temp.length;i++) {
+            num[left]=temp[i];
+            left++;
+        }
+        return a+b+sum;
+    }
+}
+
