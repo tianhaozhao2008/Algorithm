c1082b20faa4bba6bd79234d25bf81b2c90943b5
ZhaoTianhao <67959240+tianhaozhao2008@users.noreply.github.com>
2021-01-20 11:41:12 +0800

    Update 53-1、在排序数组中查找数字.java

--- 刷题群/53-1、在排序数组中查找数字.java
+++ 刷题群/53-1、在排序数组中查找数字.java
@@ -18,3 +18,39 @@ public class Solution {
         return -1;
     }
  }
+
+方法1：我的做法，是先二分，如果有重复值的话，那么查找到的位置就不一定是哪一个了，那就分别再向左和向右遍历，统计重复了几次。
+时间复杂度O（logn + k） k是重复的次数。 空间复杂度O（1）。
+class Solution {
+    public int search(int[] nums, int target) {
+        int left=0;
+        int right=nums.length-1;
+        int index=-1; //target的下标,默认是-1代表没有。
+        while(left<=right){
+            int mid=(left+right)/2;
+            if(target==nums[mid]){
+                index=mid;
+                break;
+            }
+            if(target>nums[mid]) left=mid+1;
+            else right=mid-1;
+        }
+        if(index==-1) return 0;
+        int sum=1;
+        for(int i=index+1;i<nums.length;i++){
+            if(nums[i]==target)sum++;
+            else break;
+        }
+        for(int i=index-1;i>=0;i--){
+            if(nums[i]==target)sum++;
+            else break;
+        }
+        return sum;
+    }
+}
+
+方法2：题解的方法。也是先二分找到下标，然后此时不是遍历找重复次数，而是分别再用两次二分来找重复的数值的左右边界。
+因此当重复次数特别多的时候比如重复了n次，那么上一种方法就退化成O（n）的时间复杂度了，而这种还是O（nlogn）。
+
+
+
