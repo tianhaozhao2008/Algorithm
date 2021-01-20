f1ef16dee1b77cc6ce4303bebbfd5e3db2f0def2
ZhaoTianhao <67959240+tianhaozhao2008@users.noreply.github.com>
2021-01-20 15:56:38 +0800

    Update 53-1、在排序数组中查找数字.java

--- 刷题群/53-1、在排序数组中查找数字.java
+++ 刷题群/53-1、在排序数组中查找数字.java
@@ -52,5 +52,61 @@ class Solution {
 方法2：题解的方法。也是先二分找到下标，然后此时不是遍历找重复次数，而是分别再用两次二分来找重复的数值的左右边界。
 因此当重复次数特别多的时候比如重复了n次，那么上一种方法就退化成O（n）的时间复杂度了，而这种还是O（nlogn）。
 
+比如要找3，现在查到3的下标是index，那么找3的左边界就是找第一个3，那么第一个3一定在[left,index]区间，就再写一个找左边界的二分查找方法，
+还是那个模子，弄个mid，只是什么时候算查找到，就是比如1，2，2，3，3，3这种情况，
+1.如果mid是3且mid左边不是3 或者mid是3且mid是最左边的了，就说明找到边界了；
+2.如果mid是3且mid左边也是3，那么就应该去左半边继续找，即令right=mid-1；
+3.如果mid不是3，那么就应该去右半边继续找，即令left=mid+1
+找有边界的方法也类似，就不解释了。这题代码如下（只看了题解的再用二分查找这个思路，具体实现细节和各种情况是我自己画图想的）：
+class Solution {
+    public int search(int[] nums, int target) {
+        int left=0;
+        int right=nums.length-1;
+        int index=-1; //target的下标,默认是-1代表没有。
+        while(left<=right){
+            int mid=(left+right)/2;
+            if(target==nums[mid]){
+                index=mid;
+                break;
+            }
+            if(target>nums[mid]) left=mid+1;
+            else right=mid-1;
+        }
+        if(index==-1) return 0;
+
+        int leftBoard=findLeft(nums,target,left,index);
+        int rightBoard=findRight(nums,target,index,right);
+        return rightBoard-leftBoard+1;
+    }
+    int findLeft(int[]nums,int target,int left,int right){
+        while(left<=right){
+            int mid=(left+right)/2;
+            if(nums[mid]==target){
+                if(mid>left){
+                    if(nums[mid-1]!=target) return mid;
+                    else right=mid-1;
+                }
+                else return mid;
+            }
+            else left=mid+1;
+        }
+        return -1;
+    }
+    int findRight(int[]nums,int target,int left,int right){
+        while(left<=right){
+            int mid=(left+right)/2;
+            if(nums[mid]==target){
+                if(mid<right){
+                    if(nums[mid+1]!=target) return mid;
+                    else left=mid+1;
+                }
+                else return mid;
+            }
+            else right=mid-1;
+        }
+        return -1;
+    }
+}
+
 
 
