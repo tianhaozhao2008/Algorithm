bfd1a24bee273a25bc98e6aa34c46ec616feb36c
ZhaoTianhao <67959240+tianhaozhao2008@users.noreply.github.com>
2021-01-21 21:51:34 +0800

    Update 58-2、左旋转字符串.java

--- 刷题群/58-2、左旋转字符串.java
+++ 刷题群/58-2、左旋转字符串.java
@@ -5,7 +5,8 @@ class Solution {
     }
 }
 
-方法2：遍历：用StringBuilder类型的可变字符串，然后一个一个字符加上去。时间空间都是O（n）。时间比上一种慢很多，上一种0ms，这种4ms。
+方法2：遍历：用StringBuilder类型的可变字符串，然后一个一个字符加上去。时间空间都是O（n）。
+这种最后要把StringBuilder转换成字符串，就需要一点时间或许。因此时间比上一种慢很多，上一种0ms，这种4ms。
 class Solution {
     public String reverseLeftWords(String s, int n) {
         StringBuilder res=new StringBuilder();
@@ -20,6 +21,7 @@ class Solution {
 }
 
 方法3：遍历：不用StringBuilder，而是每次新创建一个字符串，然后新拼接起来前两个。
+这种时间空间也是O（n），但这种要创建n次新的字符串、申请内存等，所以慢。
 这种更慢，这个执行要70ms，上个执行要4ms，上上个执行要0ms。但是三个占的空间都基本相同。
 class Solution {
     public String reverseLeftWords(String s, int n) {
