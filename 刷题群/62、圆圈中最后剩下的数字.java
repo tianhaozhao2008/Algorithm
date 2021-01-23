933d7cd803927b377703eb94cb6cca0d67383bb3
ZhaoTianhao <67959240+tianhaozhao2008@users.noreply.github.com>
2021-01-23 20:03:44 +0800

    Update 62、圆圈中最后剩下的数字.java

--- 刷题群/62、圆圈中最后剩下的数字.java
+++ 刷题群/62、圆圈中最后剩下的数字.java
@@ -1,6 +1,45 @@
 这题狗得很，这题应该换成困难。
 如果用环状链表模拟，时间复杂度就是O（n*m），空间复杂度O（n）。
 如果用数组模拟，如果删除元素的话标为-1，那么时间复杂度还是O（n*m），因为每次遍历要删除的元素时都要判断是否为-1。
+先写个自创链表的方法：
+class Node{
+    int value;
+    Node next;
+    Node(int value){
+        this.value=value;
+    }
+}
+class Solution {
+    public int lastRemaining(int n, int m) {
+        Node head=new Node(0);
+        Node x=head;
+        Node last=x; //这块其实不用初始化，但是后面非说might not be initial，所以随便弄一个
+        //下面for循环是创建整个链表
+        for(int i=1;i<n;i++){
+            x.next=new Node(i);
+            x=x.next;
+            if(i==n-1) {
+                x.next=head;
+                last=x;
+            }
+        }
+        if(m==1){  //m==1时单独考虑，因为后面的代码在m=1时用不了
+            for(int i=1;i<=n-1;i++){  //删除n-1次，最后只留下一个
+                last.next=last.next.next;
+            }
+            return last.value;
+        }
+        x=head;
+        for(int i=1;i<=n-1;i++){   //删除n-1次，最后只留下一个
+            for(int j=1;j<=m-2;j++){  //前移m-1次
+                x=x.next;
+            }
+            x.next=x.next.next;
+            x=x.next;
+        }
+        return x.value;
+    }
+}
 
 
 下面这种是找规律、找递推公式，dp[n]表示当还有n个元素存在时，最终能存留的那个元素的下标。从左到右数、第一个报数的在第一个。
