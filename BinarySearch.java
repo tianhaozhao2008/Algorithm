547d439e3999096441a96ff8813bb2a22b910907
ZhaoTianhao <67959240+tianhaozhao2008@users.noreply.github.com>
2021-01-31 09:54:20 +0800

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,6 +1,37 @@
+public class Solution {
+    int search(int[]num,int x,int left,int right){
+        while(left<=right){
+            int mid=(left+right)/2;
+            if(num[mid]==x)return mid;
+            if(x>num[mid]) left=mid+1;
+            else right=mid-1;
+        }
+        return -1;
+    }
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 二分查找我是用递归实现的。当然不递归也可以，构造函数就不用传入下标了，具体就不说了。
-我想说的就是对于递归函数，如果有返回值的话，那么调用自身的时候一定前面加上return，不然返回不过来（否则调用的子函数的返回值，不会反回到当前函数的返回值，因此你会看不到子函数的返回值）。
+我想说的就是对于递归函数，如果有返回值的话，那么调用自身的时候一定前面加上return，
+不然返回不过来（否则调用的子函数的返回值，不会反回到当前函数的返回值，因此你会看不到子函数的返回值）。
  */
 
 public class BinarySearch {
