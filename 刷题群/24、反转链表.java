c883165ac80b154cfd374c24c8ff5696881e399f
ZhaoTianhao <67959240+tianhaozhao2008@users.noreply.github.com>
2021-02-05 11:33:53 +0800

    Update 24、反转链表.java

--- 刷题群/24、反转链表.java
+++ 刷题群/24、反转链表.java
@@ -2,7 +2,7 @@
 （关于引用和对象的关系要搞清楚，引用.value或引用.next就是对象本尊了，而引用本身只是绳子，因此再切换绳子和对象的时候要搞清楚）
 
 注意边界条件：第一个节点特殊处理因为要指向NULL，最后一个节点也特殊处理因为没有next节点了
-注意特殊情况：只有一个节点或者输入的是null 空节点。
+注意特殊情况：只有一个节点或者输入的是 null 空节点。
 
 class Solution {
     public ListNode reverseList(ListNode head) {
