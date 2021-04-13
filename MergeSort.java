d773cbc64b9b7ebfd3b0b1bbfa68b8607c31c065
ZhaoTianhao <67959240+tianhaozhao2008@users.noreply.github.com>
2021-04-13 21:10:11 +0800

    Update MergeSort.java

--- MergeSort.java
+++ MergeSort.java
@@ -1,36 +1,36 @@
-class Solution {
-    public void sort(int[]list,int left,int right){
+这个没什么难的，不像快排和选择有坑。
+这个就是先sort然后合并即可。不用注意应该是不会写错的呵呵。
+
+void sort(int[]nums,int left,int right){
         if(left>=right) return;
         int mid=(left+right)/2;
-        sort(list,left,mid);
-        sort(list,mid+1,right);
-        
-        int []temp=new int[right-left+1];
-        int i=left; 
+        sort(nums,left,mid);
+        sort(nums,mid+1,right);
+
+        int[]temp=new int[right-left+1];
+        int i= left;
         int j=mid+1;
-        for(int m=0;m< temp.length;m++){
+        for(int m=0;m<temp.length;m++){
             if(i==mid+1){
-                temp[m]=list[j];
+                temp[m]=nums[j];
                 j++;
-                continue;
             }
-            if(j==right+1){
-                temp[m]=list[i];
-                i++;
-                continue;
-            }
-            if(list[i]<list[j]){
-                temp[m]=list[i];
+            else if(j==right+1){
+                temp[m]=nums[i];
                 i++;
             }
-            else{
-                temp[m]=list[j];
+            else if(nums[i]<nums[j]){
+                temp[m]=nums[i];
+                i++;
+            }
+            else {
+                temp[m]=nums[j];
                 j++;
             }
         }
-        for(i=0;i< temp.length;i++){
-            list[left]=temp[i];
-            left++;
+        int start=left;
+        for(int x:temp){
+            nums[start]=x;
+            start++;
         }
     }
-}
